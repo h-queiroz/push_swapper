100006b9f8f04b651364c60130a59bef48babac2
h-queiroz <hectorqrs@live.com>
2026-06-24 15:18:44 -0300

    refactor: Separated the initialization of numbers in a whole new function

--- main.c
+++ main.c
@@ -8,27 +8,45 @@
 // 4. Listá-los como está no PDF ----------------------------------------- **DONE**
 // 5. Criar comandos e fazê-los funcionar --------------------------------
 
+static int	*init_nums(int ac, char **av);
 static void	list_nums(const int *nums);
 
 int	main(int ac, char **av)
 {
-	int tmp[] = {1, 2, 3, 4};
 	int	*nums;
-	int	i;
 
-	if (ac <= 1)
-		nums = &(tmp[0]);
-	else
-	{
-		nums = calloc((ac - 1), sizeof(int));
-		i = 0;
-		while (av[++i])
-			nums[i - 1] = atoi(av[i]);
-	}
+	nums = init_nums(ac, av);
 	list_nums(nums);
+	free(nums);
 	return (0);
 }
 
+// Inicia a lista de números
+// Se receber parâmetros de números, os adiciona a lista
+// Se NÃO receber parâmetro nenhum, começa com uma lista inicial de valor {1, 2, 3, 4}
+// Não estou fazendo checagem de números válidos ainda
+static int	*init_nums(int ac, char **av)
+{
+	int	i;
+	int *nums;
+
+	i = 0;
+	if (ac <= 1)
+	{
+		nums = calloc(4, sizeof(int));
+		while ((i++) < 4)
+			nums[i - 1] = i;
+	}
+	else
+	{
+		nums = calloc((ac - 1), sizeof(int));
+		while (av[++i])
+			nums[i - 1] = atoi(av[i]);
+	}
+	return (nums);
+}
+
+// Printa a lista de números de maneira bem formatada
 static void	list_nums(const int *nums)
 {
 	printf(RED "##################################\n");
