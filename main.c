7bbcf1218c4be4ee151adfc8047d47eb69f16290
h-queiroz <hectorqrs@live.com>
2026-06-24 17:29:56 -0300

    impr: Restructured nums to work with both stacks in the same struct

--- main.c
+++ main.c
@@ -1,60 +1,103 @@
-#include "push_swapper.h"
+#include "push_swapper.h"	// COLORS Macro, t_stacks
 #include <stdio.h>			// printf()
 #include <stdlib.h>			// calloc(), free(), atoi()
 
 // 1. Receber números ---------------------------------------------------- **DONE**
 // 2. Validar que são números (A decidir o que fazer com não números) ----
-// 3. Colocá-los em uma lista -------------------------------------------- **DONE**
-// 4. Listá-los como está no PDF ----------------------------------------- **DONE**
-// 5. Criar comandos e fazê-los funcionar --------------------------------
+// 3. Colocá-los em uma stackA ------------------------------------------- **DONE**
+// 4. Criar stackB de tamanho idêntico preenchida por zeros -------------- **DONE**
+// 5. Listar stacks bem formatado como está no PDF ----------------------- **DONE**
+// 6. Receber comandos ---------------------------------------------------
+// 7. Fazer comandos funcionar -------------------------------------------
 
-static int	*init_nums(int ac, char **av);
-static void	list_nums(const int *nums);
+static t_stacks	init_stacks(int ac, char **av);
+static void	list_nums(t_stacks stacks);
 
 int	main(int ac, char **av)
 {
-	int	*nums;
+	t_stacks stacks;
 
-	nums = init_nums(ac, av);
-	list_nums(nums);
-	free(nums);
+	stacks = init_stacks(ac, av);
+	list_nums(stacks);
+	free(stacks.s_a);
+	free(stacks.s_b);
 	return (0);
 }
 
-// Inicia a lista de números
-// Se receber parâmetros de números, os adiciona a lista
-// Se NÃO receber parâmetro nenhum, começa com uma lista inicial de valor {1, 2, 3, 4}
-// Não estou fazendo checagem de números válidos ainda
-static int	*init_nums(int ac, char **av)
+// Maneira complicada de printar
+// { 1, 2, 3, 4 }
+// { 0, 5, 6, 7 }
+//
+// Maneira simples de printar
+// { 4, 3, 2, 1 }
+// { 7, 6, 5, 0 }
+//
+// 1
+// 2	5
+// 3	6
+// 4	7
+//
+// Inicia a StackA com números e StackB do mesmo tamanho mas zerada
+// Se receber parâmetros de números, os adiciona a StackA
+// Se NÃO receber parâmetro nenhum, começa a StackA com valores {4, 3, 2, 1}
+//
+// OBS1: Os números são inseridos nos arrays "ao contrário". Ou seja,
+// os valores do FUNDO da stack estão no COMEÇO do array,
+// enquanto os do TOPO se encontram ao FINAL do array
+// Fiz desta maneira para facilitar o modo de se printar os números na vertical
+//
+// OBS2: Não estou fazendo checagem de números válidos ainda
+static t_stacks	init_stacks(int ac, char **av)
 {
+	t_stacks stacks;
 	int	i;
-	int *nums;
+	int	j;
 
-	i = 0;
+	j = 1;
 	if (ac <= 1)
 	{
-		nums = calloc(4, sizeof(int));
-		while ((i++) < 4)
-			nums[i - 1] = i;
+		stacks.s_a = calloc(TMP_LEN, sizeof(int));
+		stacks.s_b = calloc(TMP_LEN, sizeof(int));
+		stacks.length = TMP_LEN;
+		i = (stacks.length - 1);
+		while (i >= 0)
+			stacks.s_a[i--] = j++;
 	}
 	else
 	{
-		nums = calloc((ac - 1), sizeof(int));
-		while (av[++i])
-			nums[i - 1] = atoi(av[i]);
+		stacks.s_a = calloc((ac - 1), sizeof(int));
+		stacks.s_b = calloc((ac - 1), sizeof(int));
+		stacks.length = (ac - 1);
+		i = (stacks.length - 1);
+		while (i >= 0)
+			stacks.s_a[i--] = atoi(av[j++]);
 	}
-	return (nums);
+	return (stacks);
 }
 
 // Printa a lista de números de maneira bem formatada
-static void	list_nums(const int *nums)
+static void	list_nums(t_stacks stacks)
 {
-	printf(RED "##################################\n");
+	int i;
+
+	i = (stacks.length - 1);
+	printf(RED "#############\n");
 	printf(GREEN "Init A and B:\n");
-	while (*nums)
-		printf(YELLOW "%5d\n", *(nums++));
+	while (i >= 0)
+	{
+		printf(YELLOW);
+		if (stacks.s_a[i] != 0)
+			printf("%5d", stacks.s_a[i]);
+		else
+			printf("\t");
+		if (stacks.s_b[i] != 0)
+			printf("%5d\n", stacks.s_b[i]);
+		else
+			printf("\n");
+		i--;
+	}
 	printf(CYAN "-----\t-----\n");
 	printf("  A\t  B\n");
-	printf(RED "##################################\n");
+	printf(RED "#############\n");
 	printf(RESET);
 }
--- push_swapper.h
+++ push_swapper.h
@@ -1,15 +1,25 @@
 #ifndef PUSH_SWAPPER_H
 # define PUSH_SWAPPER_H
 
+typedef struct s_stacks
+{
+	int	*s_a;
+	int	*s_b;
+	int	length;
+}	t_stacks;
+
+// Default stack_a length if no arguments are provided
+# define TMP_LEN 4
+
 // COLOR MACROS
-#define BLACK	"\033[1;30m"
-#define RED		"\033[1;31m"
-#define GREEN	"\033[1;32m"
-#define YELLOW	"\033[1;33m"
-#define BLUE	"\033[1;34m"
-#define MAGENTA	"\033[1;35m"
-#define CYAN	"\033[1;36m"
-#define WHITE	"\033[1;37m"
-#define RESET	"\033[0m"
+# define BLACK	"\033[1;30m"
+# define RED		"\033[1;31m"
+# define GREEN	"\033[1;32m"
+# define YELLOW	"\033[1;33m"
+# define BLUE	"\033[1;34m"
+# define MAGENTA	"\033[1;35m"
+# define CYAN	"\033[1;36m"
+# define WHITE	"\033[1;37m"
+# define RESET	"\033[0m"
 
 #endif // PUSH_SWAPPER_H
