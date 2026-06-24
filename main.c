120d9c7fcc1f9379b237a35723147b03c13f1bdd
h-queiroz <hectorqrs@live.com>
2026-06-24 13:16:50 -0300

    impr: List deals with numbers now instead of strings

--- main.c
+++ main.c
@@ -1,40 +1,40 @@
 #include "push_swapper.h"
-#include <stdio.h> // printf()
+#include <stdio.h>			// printf()
+#include <stdlib.h>			// calloc(), free(), atoi()
 
-// 1. Receber números
-// 2. Validar que são números (A decidir o que fazer com não números)
-// 3. Colocá-los em uma lista
-// 4. Listá-los como está no PDF
-// 5. Criar comandos e fazê-los funcionar
+// 1. Receber números ---------------------------------------------------- **DONE**
+// 2. Validar que são números (A decidir o que fazer com não números) ----
+// 3. Colocá-los em uma lista -------------------------------------------- **DONE**
+// 4. Listá-los como está no PDF ----------------------------------------- **DONE**
+// 5. Criar comandos e fazê-los funcionar --------------------------------
 
-void	list_nums(char **nums);
+static void	list_nums(const int *nums);
 
 int	main(int ac, char **av)
 {
-	// int		*nums;
-	char	**nums_str;
-	// Listar argumentos, mesmo que não receba nenhum
-	nums_str = (av + 1);
-	if (ac == 1)
-	{
-		*(nums_str + 0) = "1";
-		*(nums_str + 1) = "2";
-		*(nums_str + 2) = "3";
-		*(nums_str + 3) = "4";
-		*(nums_str + 4) = NULL;
-	}
-	
-	list_nums(nums_str);
+	int tmp[] = {1, 2, 3, 4};
+	int	*nums;
+	int	i;
 
+	if (ac <= 1)
+		nums = &(tmp[0]);
+	else
+	{
+		nums = calloc((ac - 1), sizeof(int));
+		i = 0;
+		while (av[++i])
+			nums[i - 1] = atoi(av[i]);
+	}
+	list_nums(nums);
 	return (0);
 }
 
-void	list_nums(char **nums_str)
+static void	list_nums(const int *nums)
 {
 	printf(RED "##################################\n");
 	printf(GREEN "Init A and B:\n");
-	while (*nums_str)
-		printf(YELLOW "%5s\n", *(nums_str++));
+	while (*nums)
+		printf(YELLOW "%5d\n", *(nums++));
 	printf(CYAN "-----\t-----\n");
 	printf("  A\t  B\n");
 	printf(RED "##################################\n");
